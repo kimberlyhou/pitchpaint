46a66f98dd8e4e12fd7d33a9b263474518fd5bc6
Noah Zweben <noahzweben@gmail.com>
2017-03-23 14:16:45 -0400

    hopefully hooked up sound buttons?

--- PitchPaint/index.ios.js
+++ PitchPaint/index.ios.js
@@ -9,23 +9,71 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  Platform,
 } from 'react-native';
+const Sound = require('react-native-sound');
+
+const c3_sound = require('sounds/C3.wav');
+
+
+const Button = ({title, onPress}) => (
+  <TouchableOpacity onPress={onPress}>
+    <Text style={styles.button}>{title}</Text>
+  </TouchableOpacity>
+);
+
+const Feature = ({title, onPress, description, buttonLabel = "PLAY"}) => (
+  <View style={styles.feature}>
+    <Header>{title}</Header>
+    <Button title={buttonLabel} onPress={onPress}/>
+  </View>
+);
+
+
+
 
 export default class PitchPaint extends Component {
+
+  this.state = {
+      loopingSound: undefined,
+    };
+
+  this.playSoundLooped = () => {
+      if (this.state.loopingSound) {
+        return;
+      }
+      const s = new Sound('sounds/C3.wav',Sound.MAIN_BUNDLE, (e) => {
+        if (e) {
+          console.log('error', e);
+        }
+        s.setNumberOfLoops(-1);
+        s.play();
+      });
+      this.setState({loopingSound: s});
+    };
+
+    this.stopSoundLooped = () => {
+      if (!this.state.loopingSound) {
+        return;
+      }
+
+      this.state.loopingSound
+        .stop()
+        .release();
+      this.setState({loopingSound: null});
+    };
+
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to PitchPaint!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Feature title="Main bundle audio (looped)" buttonLabel={'PLAY'} onPress={this.playSoundLooped}/>
+        <Feature title="Main bundle audio (looped)" buttonLabel={'STOP'} onPress={this.stopSoundLooped}/>
       </View>
     );
   }
@@ -48,6 +96,19 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  button: {
+    fontSize: 20,
+    backgroundColor: 'silver',
+    padding: 5,
+  },
+  header: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+  },
+  feature: {
+    padding: 20,
+    alignSelf: 'stretch',
+  }
 });
 
 AppRegistry.registerComponent('PitchPaint', () => PitchPaint);
