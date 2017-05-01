75a76a4a3331a9f6efe56c3a57d32375941f8f1d
jedabest <kevingarnett95@dyn-160-39-233-254.dyn.columbia.edu>
2017-04-30 22:46:02 -0400

    Added mocha and command line representation dispatch

--- client/views/test.js
+++ client/views/test.js
@@ -1,5 +1,14 @@
 console.log("cheeses");
+
 // Ewan's Notes / Test starting point:
+
+// To unit test track extractor:
+// 	-Unit test the converter from the canvas to generate a track
+// 	-Can't exhaustively test because there are infinite possiblities
+// 	-Test (8 cases in all):
+// 		-empty canvas, full canvas, one line, intersecting lines, invalid 		
+// 		files (negative values for RGB), one line around the boundary 		
+// 		(or in the corners), two pitches playing at the same time  
 
 // To unit test the renderer (takes "score" and output sounds):
 // 	-Test cases:
@@ -32,5 +41,6 @@ console.log("cheeses");
 // te.extractor([..])
 // res = ms.get()
 // assert(GGGGGG)
+
 
 
