2c6babd027bd0d537161f319085f62cc7b2e02de
jedabest <kevingarnett95@dyn-160-39-233-254.dyn.columbia.edu>
2017-04-30 20:43:25 -0400

    Noted structure to testing file beased on Ewan's OH

--- client/views/test.js
+++ client/views/test.js
@@ -1,1 +1,36 @@
 console.log("cheeses");
+// Ewan's Notes / Test starting point:
+
+// To unit test the renderer (takes "score" and output sounds):
+// 	-Test cases:
+// 		-empty track, track with one notes, track with two simultaneous 		notes,
+// 	-How to test:
+// Test Renderer()
+// 	r = new Renderer()
+// 	mockS = new MockSpeaker()
+// 	r.setSpeaker(mockS)
+
+// 	input = {red, ......}
+// 	r.render(input)
+// 	result = mockS.getCalls()
+// 	assert(res = [G.......]
+
+
+
+// What we need to change:
+// 	-Renderer does not output sounds directly, rather it calls the sounds
+
+// How to integration test:
+
+// IntTest()
+// te = newTE()
+// r = new Renderer()
+// ms = new MS()
+// te.setR(r)
+// r.setS(ms)
+
+// te.extractor([..])
+// res = ms.get()
+// assert(GGGGGG)
+
+
