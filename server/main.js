445172318495a32e5d7912a34f38525f7562cb98
Ryan DeCosmo <ryandecosmo@gmail.com>
2017-05-02 00:04:26 -0400

    schema

--- server/main.js
+++ server/main.js
@@ -1,5 +1,30 @@
 import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  db = new Mongo.Collection('db');
+
+  db.schema = new SimpleSchema({
+  username: {type: String},
+  title: {type: String, defaultValue: null, optional: true},
+  file: {type: Object, defaultValue: null, optional: true},
+  palette: {type: Object, defaultValue: null, optional: true},
+});
+
+db.insert({username: 'Ryan',
+title: 'BoyWithApple',
+file: null,
+palette: null})
+
+  const test = {
+    username: 'Ryan',
+    title: 'BoyWithApple',
+    file: null,
+    palette: null
+  }
+
+  db.schema.validate(test);
+
+
 });
